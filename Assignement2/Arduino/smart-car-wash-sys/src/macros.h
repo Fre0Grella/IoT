2fa34b89b271d63913a372308484fa08689d3a29
Marco Galeri <marco.galeri@studio.unibo.it>
2023-11-21 18:54:33 +0100

    initialize macros

---
+++ Assignement2/Arduino/smart-car-wash-sys/src/macros.h
@@ -0,0 +1,14 @@
+#ifndef __MACROS__
+#define __MACROS__
+
+const int N1 = 2;
+const int N2 = 5;
+const int N3 = 10;
+const int N4 = N2;
+/*
+const int MIN_DIST = ___ ;
+const int MAX_DIST = ___ ;
+const float MAX_TEMP = ___ ;
+*/
+
+#endif
